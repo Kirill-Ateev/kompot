55b44f216f54596fdde1ea7d723322fd752f02d5
BGDSHKA <kirill.ateev59@gmail.com>
2021-03-16 13:26:25 +0300

    feature add parameters fields and design

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
 import "./App.css";
 import React, { useState } from "react";
 import Markov from "markov-strings";
-import nGram from 'n-gram';
+import nGram from "n-gram";
+import { Button, Container, TextField, Typography } from "@material-ui/core";
+import LinearProgress from "@material-ui/core/LinearProgress";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  btn: {
+    background: "#ba0000",
+    width: "100%",
+    marginBottom: theme.spacing(1),
+  },
+  fields: {
+    width: "33.3%",
+    marginBottom: theme.spacing(1),
+    "& label.Mui-focused": {
+      color: "#ba0000",
+    },
+    "& .MuiInput-underline:after": {
+      borderBottomColor: "#ba0000",
+    },
+    "& .MuiOutlinedInput-root": {
+      "& fieldset": {
+        borderColor: "silver",
+      },
+      "&:hover fieldset": {
+        borderColor: "#ba0000",
+      },
+      "&.Mui-focused fieldset": {
+        borderColor: "#ba0000",
+      },
+    },
+  },
+}));
 
 function App() {
   const [data, setData] = useState("");
   const [result, setResult] = useState("");
   const [markov, setMarkov] = useState(null);
+  const [btn, setBtn] = useState(false);
+  const [files, setFiles] = useState([]);
+  const [load, setLoad] = useState(false);
+  const [values, setValues] = useState({
+    numberOfLines: 5,
+    stateSize: 3,
+    nGrams: 2,
+  });
+  const classes = useStyles();
 
-  const onChangeHandler = (event) => {
-    console.log(event.target.files[0]);
+  const onChangeInputHandler = (event) => {
     if (event.target.files[0]) {
+      setFiles(event.target.files);
+
       let reader = new FileReader();
 
       reader.readAsText(event.target.files[0]);
 
+      reader.onprogress = function () {
+        setLoad(true);
+      };
+
       reader.onload = function () {
-        console.log(nGram(3)(reader.result.split(/[ ]|[.]|[\r\n]+/g).filter(x => x !== '' && x !== ' ')).map(i => i.join(' ').trim()));
-        setData(nGram(4)(reader.result.split(/[.]|[\r\n]+/g).filter(x => x !== '' && x !== ' ')).map(i => i.join(' ').trim()));
+        //console.log(nGram(2)(reader.result.split(/[ ]|[.]|[\r\n]+/g).filter(x => x !== '' && x !== ' ')).map(i => i.join(' ').trim()));
+        setData(
+          nGram(values.nGrams)(
+            reader.result
+              .split(/[.]|[\r\n]+/g)
+              .filter((x) => x !== "" && x !== " ")
+          ).map((i) => i.join(" ").trim())
+        );
+        setLoad(false);
+        setBtn(true);
       };
 
       reader.onerror = function () {
@@ -26,10 +80,10 @@ function App() {
     }
   };
 
-  const onClickHandler = () => {
+  const onUploadHandler = () => {
+    debugger
     // Build the Markov generator
-    const markov = new Markov({ stateSize: 3 });
-
+    const markov = new Markov({ stateSize: values.stateSize });
     // Add data for the generator
     markov.addData(data);
     setMarkov(markov);
@@ -37,40 +91,90 @@ function App() {
 
   const generate = () => {
     let result = "";
+    try {
+      const options = {
+        maxTries: 300, // Give up if I don't have a sentence after 20 tries (default is 10)
+        // You'll often need to manually filter raw results to get something that fits your needs.
+        filter: (result) => {
+          return result; // Filter options
+        },
+      };
 
-    const options = {
-      maxTries: 30, // Give up if I don't have a sentence after 20 tries (default is 10)
-
-      // You'll often need to manually filter raw results to get something that fits your needs.
-      filter: (result) => {
-        return result; // End sentences with a dot.
-      },
-    };
-
-    // Generate a sentence
-    for (let i = 0; i < 5; i++) {
-      // console.log(markov.generate(options))
-      result = result +"\n"+ markov.generate(options).string;
+      // Generate a sentence
+      for (let i = 0; i < values.numberOfLines; i++) {
+        result = result + "\n" + markov.generate(options).string;
+      }
+      setResult(result);
+    } catch (error) {
+      console.log(error);
     }
-    console.log(result);
-    setResult(result);
+  };
+
+  const handleValueChange = (event, name) => {
+    setValues({ ...values, [name]: event.target.value });
+  };
+
+  const renderTextField = (name, label) => {
+    return (
+      <TextField
+        type="number"
+        value={values[name]}
+        InputProps={{ inputProps: { min: 0, max: 10 } }}
+        className={classes.fields}
+        id="outlined-basic"
+        onChange={(event) => handleValueChange(event, name)}
+        label={label}
+        variant="outlined"
+      />
+    );
   };
 
   return (
-    <div className="App">
-      <input
-        type="file"
-        accept="text/plain"
-        name="file"
-        onChange={onChangeHandler}
-      />
-      <button onClick={onClickHandler}>Upload</button>
-      <button onClick={generate}>Generate</button>
-      <div>STRING</div>
-      <div>{result}</div>
-      {/* REFS
-      <div>{result.refs.map((a) => `(${Object.values(a)})`).join(", ")}</div> */}
-    </div>
+    <>
+      {load && <LinearProgress color="secondary" />}
+      <Container className="app" maxWidth="sm">
+        <div className="fileInput">
+          <div className="form-group">
+            <label className="label">
+              <span className="title">
+                {files[0] ? files[0].name : "ADD TEXT FILE"}
+              </span>
+              <input
+                type="file"
+                accept="text/plain"
+                onChange={onChangeInputHandler}
+              />
+            </label>
+          </div>
+        </div>
+
+        {renderTextField("numberOfLines", "Number of lines")}
+        {renderTextField("stateSize", "State size")}
+        {renderTextField("nGrams", "N-grams")}
+
+        <Button
+          className={classes.btn}
+          variant="contained"
+          color="secondary"
+          disabled={!btn}
+          onClick={onUploadHandler}
+        >
+          {" "}
+          Upload
+        </Button>
+        <Button
+          className={classes.btn}
+          variant="contained"
+          color="secondary"
+          disabled={!btn}
+          onClick={generate}
+        >
+          Generate
+        </Button>
+
+        <Typography>{result}</Typography>
+      </Container>
+    </>
   );
 }
 
